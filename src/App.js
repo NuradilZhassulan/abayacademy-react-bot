88b28d1181d90043de2bf758392ad448e4ec0d54
Nuradil Zhasulan <nurikzhasulan96@gmail.com>
2023-07-24 20:36:25 +0600

    new file:   netlify.toml #initial 	modified:   package-lock.json #initial 	modified:   package.json #initial 	modified:   src/App.css #initial 	modified:   src/App.js #initial 	new file:   src/components/Button/Button.css #initial 	new file:   src/components/Button/Button.jsx #initial 	new file:   src/components/Form/Form.css #initial 	new file:   src/components/Form/Form.jsx #initial 	new file:   src/components/Header/Header.css #initial new file:   src/components/Header/Header.jsx #initial 	new file:   src/hooks/useTelegram.js #initial 	modified:   src/index.js #initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+// import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {onToggleButton, tg} = useTelegram();
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                {/* <Route index element={<ProductList />}/> */}
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
